54f89a69bef2228cbc05056cae85c838a14773f8
thumminbird14 <tanyapurcell@hotmail.co.uk>
2022-02-22 17:28:18 +0000

    commit answers b1-5

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -55,10 +55,16 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+print(users["Jonathan"]["twitter"])
 # 2. Get Erik's hometown
+print(users["Erik"]["home_town"])
 # 3. Get the array of Erik's lottery numbers
+print(users["Erik"]["lottery_numbers"])
 # 4. Get the species of Avril's pet Monty
+print(users["Avril"]["pets"][0]["species"])
 # 5. Get the smallest of Erik's lottery numbers
+# ln=users["Erik"]["lottery_numbers"]
+print(min(users["Erik"]["lottery_numbers"]))
 # 6. Return an array of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
